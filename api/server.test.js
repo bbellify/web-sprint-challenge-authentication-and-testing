8733f3ba5562d5e299f300ffa68e18bdfdeaaba9
Brian Bellinger <brianpatrickbellinger@gmail.com>
2021-12-17 02:42:23 -0800

    first endpoint tests passing

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,42 @@
-// Write your tests here
+const request = require('supertest')
+const server = require('./server')
+const db = require('../data/dbConfig')
+
+beforeAll(async () => {
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
+
+afterAll(async () => {
+  await db.destroy()
+})
+
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
+})
+
+
+describe('auth router', () => {
+  describe('[POST], /register', () => {
+    let res
+    beforeEach(async () => {
+      res = await request(server)
+        .post('/api/auth/register')
+        .send({ username: 'mister', password: 'sister' })
+    })
+    it('responds with 201 created and new user', async () => {
+      expect(res.status).toBe(201)
+      expect(res.body).toMatchObject({ username: 'mister' })
+    })
+    it('creates a new user in db', async () => {
+      const [newUser] = await db('users').where('id', 1)
+      expect(newUser).toMatchObject({ username: 'mister' })
+    })
+  })
+
+})
+
+
+describe('login endpoint', () => {
+
 })
